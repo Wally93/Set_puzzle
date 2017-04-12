0c52371e7827b29de00043ec42e39126b017f0b5
Wally Jniyah <ojniyah@gmail.com>
2017-04-12 06:59:28 -0400

    found a way to check if 3 cards are selected

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 
-
+/* check if its a set or not*/
 var object1 = { color:"red", shape:"square", number:1, shading:"full"};
 var object2 = { color:"green", shape:"circle", number:2, shading: "empty"};
 var object3 = { color:"blue", shape:"triangle", number:3, shading: "shred"};
@@ -21,3 +21,37 @@ alert("yes its a SET");
 else {
   alert("no its not a set");
 }
+
+  /* bind data to the cards */
+$(document).ready(function(){
+  $(".card1").data({ color:"red", shape:"squiggle", number:1, shading:"full"});
+  $(".card2").data({ color:"green", shape:"squiggle", number:2, shading:"full"});
+  $(".card3").data({ color:"green", shape:"diamond", number:1, shading:"full"});
+  $(".card4").data({ color:"green", shape:"diamond", number:3, shading:"full"});
+  $(".card5").data({ color:"green", shape:"oval", number:1, shading:"full"});
+  $(".card6").data({ color:"red", shape:"oval", number:3, shading:"shred"});
+  $(".card7").data({ color:"purple", shape:"oval", number:2, shading:"shred"});
+  $(".card8").data({ color:"green", shape:"oval", number:1, shading:"shred"});
+  $(".card9").data({ color:"purple", shape:"squiggle", number:3, shading:"empty"});
+  $(".card10").data({ color:"green", shape:"squiggle", number:1, shading:"empty"});
+  $(".card11").data({ color:"green", shape:"diamond", number:3, shading:"empty"});
+  $(".card12").data({ color:"purple", shape:"diamond", number:1, shading:"empty"});
+
+  $(".card3").click(function(){
+      alert($(".card9").data("color"));
+  });
+
+
+
+  /* alert me when 3 checkboxes are checked */
+  $("input[type=checkbox]").click(function CountCheckedBoxes(){
+      if($("input[type=checkbox]:checked").length > 2){
+      alert("3 cards are checked now!");
+       }
+       else {
+      alert("3 cards not checked yet");
+      }
+      });
+});
+
+/* now I'm trying to bind every checkbox to its card, and then find a way to compare the 3 selected cards to each other*/
